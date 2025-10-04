2ca395259fa97a68b8c642c5d54ce5cbd5988432
Saran <saransubu201@gmail.com>
2025-10-04 20:26:00 +0530

    your clear message here

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ let bodyParser = require("body-parser");
 let app = express();
 let dbcon = require("./database");
 let ticApp = require("./ticketapi");
+let path = require("path");
 let UserApp = require("./userapi");
 let ImageApp = require("./imageapi");
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
+
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.use("/image-uploads", express.static(path.join(__dirname, "../uploads")));
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
